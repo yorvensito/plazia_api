5b534afebc518d30dda346805a19f93241f108f6
George Alvarado <galvarado@solucionamericana.com.co>
2025-07-11 13:45:46 -0500

    added migration

--- favorites/models.py
+++ favorites/models.py
@@ -7,6 +7,7 @@ from users.models import CustomUser
 class Favorite(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)  # 👈 este campo es necesario
 
     class Meta:
         unique_together = ("user", "place")
