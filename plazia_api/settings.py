4bf23c61368529ff766f5e9e9a7ce98a3e33ee51
George Alvarado <galvarado@solucionamericana.com.co>
2025-07-11 17:32:32 -0500

    fix: update settings

--- plazia_api/settings.py
+++ plazia_api/settings.py
@@ -141,8 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+if DEBUG:
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+else:
+    STATIC_ROOT = "/opt/render/project/src/staticfiles"
 STATIC_URL = "/static/"
 
 # Default primary key field type
